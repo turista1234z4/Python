267cc2d51bb38645c9934d3bd314236ad477786c
turista1234z4 <113246150+turista1234z4@users.noreply.github.com>
2024-02-07 09:51:34 -0300

    Add files via upload

---
+++ dia da semana.py
@@ -0,0 +1,18 @@
+dia=int(input("digite um número de 1 a 7: "))
+if dia<1 or dia>7:
+    print("o número não é valido")
+if dia==1:
+    print("Domingo")
+if dia==2:
+    print("Segunda")
+if dia==3:
+    print("Terça")
+if dia==4:
+    print("Quarta")
+if dia==5:
+    print("Quinta")
+if dia==6:
+    print("Sexta")
+if dia==7:
+    print("Sábado")
+    
